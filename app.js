bb2fc7211984cb221592bb45209fb3084ffadd2f
FAdam123 <130734367+FAdam123@users.noreply.github.com>
2023-05-18 20:06:05 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const hamburger = document.getElementById('hamburger');
 const nav = document.getElementById('nav');
 var icon = document.getElementById('myIcon');
 var button = document.getElementById('myButton');
+var phoneInput = document.getElementById("phoneInput");
+var input = document.getElementById("myInput");
 
 hamburger.addEventListener('click', function (event) {
     nav.classList.toggle('menu-active')
@@ -13,3 +15,10 @@ hamburger.addEventListener('click', function (event) {
 hamburger.addEventListener('click', function (event) {
   hamburger.classList.toggle('fa-xmark');
 });
+
+phoneInput.addEventListener("input", function() {
+    var phoneNumber = phoneInput.value;
+    var sanitizedNumber = phoneNumber.replace(/\D/g, "");
+    phoneInput.value = sanitizedNumber;
+});
+
